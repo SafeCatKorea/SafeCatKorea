31951854d90d1b347af5120bcd18e693489dcfa0
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-07-16 00:46:28 +0900

    [init] react-springboot 통합 환경 구축
    *백엔드*
    사용 언어: Java
    프레임워크: SpringBoot
    자바 버전: 17
    스프링부트 버전: 3.3.1
    빌드관리: Gradle
    Dependencies: springWeb
    
    *프론트엔드*
    사용언어: js
    프레임워크: React
    전역상태관리 라이브러리: Mobx(예정)
    npm: 10.8.2(최신 버전)

---
+++ src/main/java/PortfolioRangers/SafeCatKorea/SafeCatKoreaApplication.java
@@ -0,0 +1,13 @@
+package PortfolioRangers.SafeCatKorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeCatKoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeCatKoreaApplication.class, args);
+	}
+
+}
