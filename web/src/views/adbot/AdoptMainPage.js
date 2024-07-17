50b65dbaf9acdb90103fac954775ea7bb7360dc8
Jae-Hong-Kim <newfly101@naver.com>
2024-07-17 22:34:39 +0900

    [입양] 입양하기 메인 페이지 추가

---
+++ web/src/views/adbot/AdoptMainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from '../../css/AdoptPage.module.css';
+
+const AdoptMain = () => {
+    return (
+        <div className={classes.contentBox}>
+            <div className={classes.adoptHeader}>
+                <h3>입양대상 동물</h3>
+            </div>
+
+        </div>
+    );
+};
+
+export default AdoptMain;
