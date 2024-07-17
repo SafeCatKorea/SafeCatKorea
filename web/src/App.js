1ed08c0048af475d0e61c8aec0d8d3ac2f694be4
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-07-17 13:57:03 +0900

    [common] 임시 Navigation 추가

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,13 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./views/Main";
+import Navigation from "./component/main/Navigation";
 
 
 function App() {
   return (
     <Router>
+      <Navigation/>
       <Routes>
         <Route path={'/'} element={<Main />} />
         {/* <Route path={url} element={<컴포넌트명 />} /> */}
