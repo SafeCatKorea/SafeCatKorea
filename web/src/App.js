0014cf8519bec703a33ef1ef4e3c5e94c391296c
Jae-Hong-Kim <newfly101@naver.com>
2024-07-17 19:03:55 +0900

    [setting] 줄바꿈 변경 : web-hook check

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,19 @@
-import {Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import "./App.css";
 import Main from "./views/Main";
 import Navigation from "./component/main/Navigation";
 
 
 function App() {
-  return (
-    <Router>
-      <Navigation/>
-      <Routes>
-        <Route path={'/'} element={<Main />} />
-        {/* <Route path={url} element={<컴포넌트명 />} /> */}
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navigation/>
+            <Routes>
+                <Route path={'/'} element={<Main/>}/>
+                {/* <Route path={url} element={<컴포넌트명 />} /> */}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
