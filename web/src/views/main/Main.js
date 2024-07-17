c9ff01658b108a42f2826987965741b778b1248e
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-07-17 21:58:22 +0900

    [Main] 임시 배경이미지 추가

--- web/src/views/main/Main.js
+++ web/src/views/main/Main.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from "../../css/main/Main.module.css"
 import MainIcons from "./MainIcons";
 
+
 const Main = () => {
   return (
     <div className={classes.main}>
-      <div className={classes.mainWrapper}>
+      <div className={classes.mainWrapper} style={{backgroundImage: `url(/mainImg.png)`}}>
         <div className={classes.mainText}>
           <h2 className={classes.title}>통합 유기묘 플랫폼</h2>
           <div className={classes.desc}>길 잃은 생명에게 나침반을</div>
