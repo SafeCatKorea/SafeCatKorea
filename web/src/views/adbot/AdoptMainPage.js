86123f615032f3f9b4b55d9c074dcd9848f3ef7c
Jae-Hong-Kim <newfly101@naver.com>
2024-07-17 23:32:26 +0900

    [입양] 입양하기 메인 페이지 추가 : category 구조 디자인 추가 ( @media로 반응형으로 작업 필요 )

--- web/src/views/adbot/AdoptMainPage.js
+++ web/src/views/adbot/AdoptMainPage.js
@@ -7,20 +7,76 @@ const AdoptMain = () => {
             <div className={classes.adoptHeader}>
                 <h3>입양대상 동물</h3>
             </div>
-
-            <div className={classes.adoptForm}>
-                <div>날짜</div>
-                <div>시도</div>
-                <div>종류</div>
-                <div>동물등록번호</div>
-                <div className={classes.hr} />
-                <div>
-                    <button>
-                        조회
-                    </button>
+            <div className={classes.subContentBox}>
+                <div className={classes.adoptForm}>
+                    <div className={classes.adoptFormGroup}>
+                        <label>날짜</label>
+                        <input type="date"
+                               name="searchSDate"
+                               id="searchSDate"
+                               title="시작일"
+                               maxLength="10"
+                               className={classes.adoptFormControl}
+                        />
+                        <div className={classes.hyphen}>~</div>
+                        <input type="date"
+                               name="searchEDate"
+                               id="searchEDate"
+                               title="종료일"
+                               maxLength="10"
+                               className={classes.adoptFormControl}
+                        />
+                        <label>(접수일 기준)</label>
+                    </div>
+                    <div className={classes.adoptFormGroup}>
+                        <label>시도</label>
+                        <select className={classes.adoptFormControl}>
+                            <option>전체</option>
+                            <option>전국</option>
+                        </select>
+                    </div>
+                    <div className={classes.adoptFormGroup}>
+                        <label>시군구</label>
+                        <select className={classes.adoptFormControl}>
+                            <option>전체</option>
+                            <option>전국</option>
+                        </select>
+                    </div>
+                    <div className={classes.adoptFormGroup}>
+                        <label>보호센터</label>
+                        <select className={classes.adoptFormControl}>
+                            <option>전체</option>
+                            <option>전국</option>
+                        </select>
+                    </div>
+                    <div className={classes.adoptFormGroup}>
+                        <label>고양이 품종</label>
+                        <select className={classes.adoptFormControl}>
+                            <option>전체</option>
+                            <option>고양이</option>
+                            <option>기타</option>
+                            <option>알수없음</option>
+                            <option>페르시안</option>
+                            <option>믹스</option>
+                        </select>
+                    </div>
+                    <div className={classes.adoptFormGroup}>
+                        <label>동물등록번호</label>
+                        <input type="text"
+                               name="searchRfid"
+                               id="searchRfid"
+                               maxLength="15"
+                               size="20"
+                               title="동물등록번호입력"
+                               className={classes.adoptFormControl}
+                        />
+                    </div>
+                    <div className={classes.hr}/>
+                    <div className={classes.adoptFormSubmit}>
+                        <button> 조회</button>
+                    </div>
                 </div>
             </div>
-
         </div>
     );
 };
