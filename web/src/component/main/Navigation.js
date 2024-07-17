015ecbef6e6be7fd3cdea7460f1f882cea1ebc44
Jae-Hong-Kim <newfly101@naver.com>
2024-07-17 18:59:01 +0900

    [setting] navigation 밑으로 padding 값 추가

--- web/src/component/main/Navigation.js
+++ web/src/component/main/Navigation.js
@@ -3,24 +3,22 @@ import {Link} from "react-router-dom";
 import classes from '../../css/component/Navigation.module.css'
 
 const Navigation = () => {
-  const menuItem = [
-    {navName: '유기묘 찾기', navUrl: '/searchCat'},
-    {navName: '유기묘 입양하기', navUrl: '/adoptCat'},
-    {navName: '유기묘 신고하기', navUrl: '/reportCat'}]
-  return (
-    <div>
-      <nav className={classes.navBar}>
-          <Link to={'/'}>로고</Link>
-          <div className={classes.navItem}>
-            {menuItem.map(item=>
-              <Link to={item.navUrl} className={classes.navItemDiv}>
-              <div>{item.navName}</div>
-              </Link>
-            )}
-          </div>
-      </nav>
-    </div>
-  );
+    const menuItem = [
+        {navName: '유기묘 찾기', navUrl: '/searchCat'},
+        {navName: '유기묘 입양하기', navUrl: '/adoptCat'},
+        {navName: '유기묘 신고하기', navUrl: '/reportCat'}]
+    return (
+        <nav className={classes.navBar}>
+            <Link to={'/'}>로고</Link>
+            <div className={classes.navItem}>
+                {menuItem.map(item =>
+                    <Link to={item.navUrl} className={classes.navItemDiv}>
+                        <div>{item.navName}</div>
+                    </Link>
+                )}
+            </div>
+        </nav>
+    );
 };
 
 export default Navigation;
--- web/src/views/Main.js
+++ web/src/views/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Main = () => {
   return (
-    <div>
+    <div style={{paddingTop:"70px"}}>
       hello world
     </div>
   );
