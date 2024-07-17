955d9abf037f57d624868b031dc88de9d07e242d
Jae-Hong-Kim <newfly101@naver.com>
2024-07-17 22:52:29 +0900

    [입양] 입양하기 메인 페이지 추가 : category구조

--- web/src/views/adbot/AdoptMainPage.js
+++ web/src/views/adbot/AdoptMainPage.js
@@ -8,6 +8,19 @@ const AdoptMain = () => {
                 <h3>입양대상 동물</h3>
             </div>
 
+            <div className={classes.adoptForm}>
+                <div>날짜</div>
+                <div>시도</div>
+                <div>종류</div>
+                <div>동물등록번호</div>
+                <div className={classes.hr} />
+                <div>
+                    <button>
+                        조회
+                    </button>
+                </div>
+            </div>
+
         </div>
     );
 };
