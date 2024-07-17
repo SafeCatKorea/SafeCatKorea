caf6b7212cb36aebf5514aff1bf7b48aadc67ea1
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-07-17 22:42:47 +0900

    [Main] 메인 페이지 기본 레이아웃

--- web/src/views/main/Main.js
+++ web/src/views/main/Main.js
@@ -8,10 +8,12 @@ const Main = () => {
     <div className={classes.main}>
       <div className={classes.mainWrapper} style={{backgroundImage: `url(/mainImg.png)`}}>
         <div className={classes.mainText}>
-          <h2 className={classes.title}>통합 유기묘 플랫폼</h2>
+          <h1 className={classes.title}>통합 유기묘 플랫폼</h1>
           <div className={classes.desc}>길 잃은 생명에게 나침반을</div>
         </div>
-        <MainIcons/>
+        <div className={classes.menuIcon}>
+          메뉴
+        </div>
       </div>
     </div>
   );
--- web/src/views/main/MainIcons.js
+++ web/src/views/main/MainIcons.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import classes from "../../css/main/MainIcons.module.css"
 
 const MainIcons = () => {
+  const icons = [
+    {
+      title: '전국 유기묘 보호센터',
+      url: '/center',
+      desc: '전국의 유기묘 보호센터 목록을 볼 수 있습니다.'
+    },
+    {
+      title: '동물병원',
+      url: '/center',
+      desc: '전국의 동물병원 목록을 볼 수 있습니다.'
+    },
+    {
+      title: '고양이 관리법',
+      url: '/center',
+      desc: '고양이 관리법에 대해 볼 수 있습니다.'
+    },
+    {
+      title: '입양 받는 절차',
+      url: '/center',
+      desc: '입양 받는 절차에 대해 볼 수 있습니다.'
+    },
+  ];
   return (
-    <div>
-
+    <div className={classes.iconWrapper}>
+      {icons.map(item=>
+        <Link to={item.url} style={{textDecoration: 'none', color: 'black' }}>
+          <div className={classes.iconContainer}>
+            <div>{item.title}</div>
+            <div>{item.desc}</div>
+          </div>
+        </Link>
+      )}
     </div>
   );
 };
