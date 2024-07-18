cce6290fa9dea3f7b78f57186c9cc6e634cc001e
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-07-18 23:52:52 +0900

    [Main] 모달 기능 구현 완료

--- web/src/views/main/Main.js
+++ web/src/views/main/Main.js
@@ -5,12 +5,45 @@ import MainIcons from "./MainIcons";
 
 const Main = () => {
   const [menuFlag, setMenuFlag] = useState(false);
+  const [modal, setModal] = useState(false);
 
   const onMouseOver = ()=>{
     setMenuFlag(true);
   }
+
+  const onClickIcon =()=>{
+    setModal(true);
+  }
+
+  const icons = [
+    {
+      title: '전국 유기묘 보호센터',
+      url: '/center',
+      desc: '전국의 유기묘 보호센터 목록을 볼 수 있습니다.'
+    },
+    {
+      title: '동물병원',
+      url: '/center',
+      desc: '전국의 동물병원 목록을 볼 수 있습니다.'
+    },
+    {
+      title: '고양이 관리법',
+      url: '/center',
+      desc: '고양이 관리법에 대해 볼 수 있습니다.'
+    },
+    {
+      title: '입양 받는 절차',
+      url: '/center',
+      desc: '입양 받는 절차에 대해 볼 수 있습니다.'
+    },
+  ];
   return (
     <div className={classes.main}>
+      {modal&&<div className={classes.iconClickModal} onClick={()=>setModal(false)}>
+        <div className={classes.modalContainer}>
+          아이콘 모달 입니다.
+        </div>
+      </div>}
       <div className={classes.mainWrapper} style={{backgroundImage: `url(/mainImg.png)`}}>
         <div className={classes.mainText}>
           <h1 className={classes.title}>통합 유기묘 플랫폼</h1>
@@ -20,7 +53,7 @@ const Main = () => {
           메뉴
         </div>
         <div className={`${classes.icons} ${!menuFlag&&classes.blankMenu}`}>
-        <MainIcons menuFlag={menuFlag}/>
+        <MainIcons menuFlag={menuFlag} icons={icons} onClickIcon={onClickIcon}/>
         </div>
       </div>
     </div>
--- web/src/views/main/MainIcons.js
+++ web/src/views/main/MainIcons.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+
 import classes from "../../css/main/MainIcons.module.css";
 
-const MainIcons = ({ menuFlag }) => {
+const MainIcons = ({ menuFlag, icons, onClickIcon}) => {
   const [animationEnded, setAnimationEnded] = useState(Array(4).fill(false));
   const [hoverIcon, setHoverIcon] = useState(Array(4).fill(false));
 
@@ -30,38 +30,18 @@ const MainIcons = ({ menuFlag }) => {
     })
   }
 
-  const icons = [
-    {
-      title: '전국 유기묘 보호센터',
-      url: '/center',
-      desc: '전국의 유기묘 보호센터 목록을 볼 수 있습니다.'
-    },
-    {
-      title: '동물병원',
-      url: '/center',
-      desc: '전국의 동물병원 목록을 볼 수 있습니다.'
-    },
-    {
-      title: '고양이 관리법',
-      url: '/center',
-      desc: '고양이 관리법에 대해 볼 수 있습니다.'
-    },
-    {
-      title: '입양 받는 절차',
-      url: '/center',
-      desc: '입양 받는 절차에 대해 볼 수 있습니다.'
-    },
-  ];
+
 
   return (
     <div className={classes.iconWrapper}>
       {icons.map((item, index) => (
-        <div style={{ textDecoration: 'none', color: 'black' }} key={index}>
+        <div key={index}>
           <div
             className={`${classes.iconContainer} ${classes[`icon${index}`]} ${animationEnded[index] && menuFlag ? classes.changed : ''} ${hoverIcon[index] && menuFlag ? classes.hoverIcon : ''}`}
             onAnimationEnd={() => handleAnimationEnd(index)}
             onMouseOver={()=>handleHoverIcon(index)}
             onMouseOut={()=>handleHoverOver(index)}
+            onClick={()=>onClickIcon()}
           >
             <div >{item.title} </div>
             <div>{hoverIcon[index]&&item.desc}</div>
