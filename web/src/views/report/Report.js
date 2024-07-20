41e1d67a5f7a2fa8664f0dfa782bafdf35fb2930
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-07-20 21:24:00 +0900

    [report] 리포트 페이지 임시 css 구현

--- web/src/views/report/Report.js
+++ web/src/views/report/Report.js
@@ -6,10 +6,6 @@ const Report = () => {
 
   return (
     <div className={classes.reportPage}>
-      <div className={classes.reportText}>
-        <div className={classes.reportTitle}>유기묘 신고하기</div>
-        <div className={classes.reportDesc}>관할 보호소에 이첩됩니다.</div>
-      </div>
       <div className={classes.reportForm}>
         <ReportForm  />
       </div>
--- web/src/views/report/ReportForm.js
+++ web/src/views/report/ReportForm.js
@@ -3,23 +3,29 @@ import ReportDropDown from "./ReportDropDown";
 import classes from "../../css/report/ReportForm.module.css"
 
 const ReportForm = () => {
-  const [dropDown, setDropDown] =useState(false);
-  const [pickItem, setPickItem] =useState('서울');
-  const localItem =['서울','부산','경남','대전','대구'];
-  const clickDropDown =()=>{
+  const [dropDown, setDropDown] = useState(false);
+  const [pickItem, setPickItem] = useState('서울');
+  const localItem = ['서울', '부산', '경남', '대전', '대구'];
+  const clickDropDown = () => {
     setDropDown(!dropDown);
   }
-  const slickItem =(item)=>{
+  const slickItem = (item) => {
     setPickItem(item);
     clickDropDown();
   }
   return (
-    <form>
-      <div className={classes.dropDown} onClick={clickDropDown}>
-        {pickItem}
-        {dropDown&&<ReportDropDown localItem={localItem} slickItem={slickItem}/>}
+    <form className={classes.reportForm}>
+        <h1 className={classes.reportTitle}>유기묘 신고하기</h1>
+      <div className={classes.reportForm1}>
+        <div className={classes.reportDesc}>유기묘 발견 지역을 선택해주세요</div>
+        <div className={!dropDown && classes.dropDown} onClick={clickDropDown}>
+          {!dropDown && pickItem}
+          {dropDown && <ReportDropDown localItem={localItem} slickItem={slickItem}/>}
+        </div>
       </div>
-
+      <div className={classes.checkBox}>
+        확인 ▶
+      </div>
 
     </form>
   );
